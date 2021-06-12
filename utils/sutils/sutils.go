e7e4df30d73558eac2fc520f34e160360d493295
Clairton Santos <hi@clairton.wtf>
2021-06-12 16:33:53 -0300

    🐛 Fix prettyTime and remove prettyMonth

--- utils/sutils/sutils.go
+++ utils/sutils/sutils.go
@@ -3,6 +3,7 @@ package sutils
 import (
 	"fmt"
 	"math"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -10,6 +11,11 @@ import (
 )
 
 var converter = md.NewConverter("", true, nil)
+
+func GetRegex(reg string) *regexp.Regexp {
+	result, _ := regexp.Compile(reg)
+	return result
+}
 
 func Fmt(s string, a ...interface{}) string {
 	return fmt.Sprintf(s, a...)
@@ -29,45 +35,14 @@ func ToHHMMSS(baseSeconds float64) string {
 	seconds := baseSeconds - hours*3600 - minutes*60
 
 	if hours >= 1 {
-		return Fmt("%02v:%02v:%02v", hours, minutes, seconds)
+		return Fmt("%02d:%02d:%02d", int(hours), int(minutes), int(seconds))
 	}
 
-	return Fmt("%02v:%02v", minutes, seconds)
+	return Fmt("%02v:%02v", int(minutes), int(seconds))
 }
 
 func SplitString(r rune) bool {
 	return r == ' ' || r == '\n'
-}
-
-func ToPrettyMonth(m int) string {
-	switch m {
-	case 1:
-		return "Jan"
-	case 2:
-		return "Fev"
-	case 3:
-		return "Mar"
-	case 4:
-		return "Abr"
-	case 5:
-		return "Mai"
-	case 6:
-		return "Jun"
-	case 7:
-		return "Jul"
-	case 8:
-		return "Ago"
-	case 9:
-		return "Set"
-	case 10:
-		return "Out"
-	case 11:
-		return "Nov"
-	case 12:
-		return "Dez"
-	default:
-		return ""
-	}
 }
 
 func ToLower(s interface{}) string {
