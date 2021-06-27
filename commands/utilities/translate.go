5daea7d79f77e76b006b4484f45eb45a39812dd1
Clairton Santos <hi@clairton.wtf>
2021-06-26 21:37:26 -0300

    🐛 Handle translated texts greater than 1024 characters

--- commands/utilities/translate.go
+++ commands/utilities/translate.go
@@ -19,12 +19,30 @@ var TranslateCommand = base.Command{
 			return
 		}
 
-		result, err := i18n.FromGoogle("auto", lineArgs[0], strings.Join(lineArgs[1:], " "))
+		result, err := i18n.FromGoogle("auto", ctx.Args[0], strings.Join(lineArgs[1:], " "))
 
 		if err != nil {
 			ctx.ReplyWithError(err)
 		} else {
-			ctx.ReplyRawWithEmote(constants.PEPEFROG, result)
+			if len(result) > 1024 {
+
+				spaceIndex := strings.LastIndex(result[:1024], " ")
+				var firstPart string
+				var secondPart string
+
+				if spaceIndex > -1 {
+					firstPart = result[:spaceIndex]
+					secondPart = result[spaceIndex:]
+				} else {
+					firstPart = result[:1024]
+					secondPart = result[1024:]
+				}
+
+				ctx.ReplyRawWithEmote(constants.PEPEFROG, firstPart)
+				ctx.SendRaw(secondPart)
+			} else {
+				ctx.ReplyRawWithEmote(constants.PEPEFROG, result)
+			}
 		}
 	},
 }
