7b4ec2fcdcee20a220e9edf5439ca66dc7c64a0a
Clairton Santos <clairtonsbj@gmail.com>
2021-09-20 18:13:02 -0300

    feat: add preview in trace context

--- interactions/image/trace.go
+++ interactions/image/trace.go
@@ -1,6 +1,7 @@
 package image
 
 import (
+	"bytes"
 	"strings"
 
 	"github.com/ItsClairton/Anny/base/discord"
@@ -35,12 +36,25 @@ var TraceContext = discord.Interaction{
 			return
 		}
 
-		ctx.EditResponse(discord.NewResponse().
-			WithContentEmoji(emojis.KannaPeer, "É uma cena%s de %s.",
-				utils.Is(result.Episode > 0, utils.Fmt(" do episódio **%d**", result.Episode), ""),
-				utils.Is(len(result.Title.English) > 0 && !strings.EqualFold(result.Title.Japanese, result.Title.English),
-					utils.Fmt("**%s** (**%s**)", result.Title.Japanese, result.Title.English),
-					utils.Fmt("**%s**", result.Title.Japanese))))
+		ctx.EditResponse(response.WithContentEmoji(emojis.KannaPeer, "É uma cena%s de %s (%s).",
+			utils.Is(result.Episode > 0, utils.Fmt(" do episódio **%d**", result.Episode), ""),
+			utils.Is(len(result.Title.English) > 0 && !strings.EqualFold(result.Title.Japanese, result.Title.English),
+				utils.Fmt("**%s** (**%s**)", result.Title.Japanese, result.Title.English),
+				utils.Fmt("**%s**", result.Title.Japanese)),
+			utils.Is(utils.ToDisplayTime(result.From) == utils.ToDisplayTime(result.To),
+				utils.Fmt("`%s`", utils.ToDisplayTime(result.From)),
+				utils.Fmt("`%s`/`%s`", utils.ToDisplayTime(result.From), utils.ToDisplayTime(result.To)))))
+
+		video, err := utils.GetFromWeb(result.Video)
+
+		if err == nil {
+			response.WithContent("").WithFile(&discordgo.File{
+				Name:        utils.Is(result.Adult, "SPOILER_preview.mp4", "preview.mp4"),
+				ContentType: "video/mp4",
+				Reader:      bytes.NewReader(video),
+			})
+			ctx.SendFollowUp(response)
+		}
 	},
 }
 
--- utils/misc.go
+++ utils/misc.go
@@ -1,7 +1,22 @@
 package utils
 
-import "math/rand"
+import (
+	"math"
+	"math/rand"
+)
 
 func RandomBool() bool {
 	return rand.Int63()&1 == 0
+}
+
+func ToDisplayTime(baseSeconds float64) string {
+	hours := math.Floor(baseSeconds / 3600)
+	minutes := math.Floor((baseSeconds - hours*3600) / 60)
+	seconds := baseSeconds - hours*3600 - minutes*60
+
+	if hours >= 1 {
+		return Fmt("%02d:%02d:%02d", int(hours), int(minutes), int(seconds))
+	}
+
+	return Fmt("%02v:%02v", int(minutes), int(seconds))
 }
