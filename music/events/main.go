d57915e8c9c9a1ac1b12dc968f3676e31e838f15
Clairton Santos <clairtonsbj@gmail.com>
2021-12-03 21:03:49 -0300

    ✨ add voice events

---
+++ music/events/main.go
@@ -0,0 +1,31 @@
+package music
+
+import (
+	"time"
+
+	"github.com/ItsClairton/Anny/core"
+	music "github.com/ItsClairton/Anny/music/audio"
+	"github.com/diamondburned/arikawa/v3/gateway"
+)
+
+var VServerUpdateEvent = core.Event{
+	Handler: func(e *gateway.VoiceServerUpdateEvent) {
+		time.Sleep(1 * time.Second)
+
+		if player := music.GetPlayer(e.GuildID); player != nil && player.State == music.PlayingState {
+			player.Voicy.SendSpeaking()
+		}
+	},
+}
+
+var VStateUpdateEvent = core.Event{
+	Handler: func(e *gateway.VoiceStateUpdateEvent) {
+		if e.UserID != core.Self.ID {
+			return
+		}
+
+		if player := music.GetPlayer(e.GuildID); player != nil && e.ChannelID.IsNull() {
+			player.Stop(false)
+		}
+	},
+}
--- music/main.go
+++ music/main.go
@@ -3,9 +3,10 @@ package music
 import (
 	"github.com/ItsClairton/Anny/core"
 	commands "github.com/ItsClairton/Anny/music/commands"
+	events "github.com/ItsClairton/Anny/music/events"
 )
 
 var Module = &core.Module{
 	Commands: []*core.Command{&commands.PlayCommand, &commands.SkipCommand, &commands.StopCommand, &commands.PauseCommand, &commands.ResumeCommand, &commands.SeekCommand, &commands.NowplayingCommand, &commands.ShuffleCommand},
-	Events:   []*core.Event{},
+	Events:   []*core.Event{&events.VServerUpdateEvent, &events.VStateUpdateEvent},
 }
